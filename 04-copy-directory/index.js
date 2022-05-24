c7fa92cfcc764d18a3f4e0bc064b54858b8e5490
Roman <kadkin.roman@gmail.com>
2022-05-25 02:21:17 +0300

    feat: add copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,26 @@
+const fs = require('fs');
+const path = require("path");
+let input, output;
 
+
+fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
+    if (err) throw err;
+    console.log('Папка была создана');
+});
+
+fs.readdir(path.join(__dirname, 'files'), (err, files) => {
+    if (err) throw err;
+
+    for (let index in files) {
+        fs.stat(path.join(__dirname, 'files', files[index]), function(err, stat) {
+            if (err) console.log(err);
+
+            if (stat.isFile(path.join(__dirname, 'files', files[index]))) {
+                console.log(path.basename(path.join(__dirname, 'files', files[index])));
+                input = fs.createReadStream(path.join(__dirname, 'files', path.basename(path.join(__dirname, 'files', files[index]))), 'utf-8');
+                output = fs.createWriteStream(path.join(__dirname, 'files-copy', path.basename(path.join(__dirname, 'files', files[index]))));
+                input.pipe(output);
+            }
+        });
+    }
+});
