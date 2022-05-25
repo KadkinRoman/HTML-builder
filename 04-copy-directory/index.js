4db3f5c2a0dde633521301186f8cc0c8d1204932
Roman <kadkin.roman@gmail.com>
2022-05-25 12:47:57 +0300

    fix: update the contents of the files-copy folder when the script is restarted

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,12 +1,37 @@
 const fs = require('fs');
 const path = require("path");
 let input, output;
+const filesCopyPath = path.join(__dirname, 'files-copy');
 
 
 fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
     if (err) throw err;
-    console.log('Папка была создана');
 });
+
+function listObjects(currentPath) {
+    fs.readdir(currentPath, (err, files) => {
+        if (err) throw err;
+
+        for (let file in files) {
+
+            fs.stat(path.join(__dirname, 'files-copy', files[file]), (err, stat) => {
+                if (err) throw err;
+
+                if (stat.isDirectory()) {
+                    listObjects(currentPath + '/' + file); // продолжаем рекурсию
+                } else {
+                    let fs = require('fs');
+                    fs.unlink(path.join(__dirname, 'files-copy', files[file]), 
+                    err => {
+                        if (err) throw err; // не удалось удалить файл
+                    });
+                }
+            });
+        }
+    });
+}
+
+listObjects(filesCopyPath);
 
 fs.readdir(path.join(__dirname, 'files'), (err, files) => {
     if (err) throw err;
@@ -16,7 +41,6 @@ fs.readdir(path.join(__dirname, 'files'), (err, files) => {
             if (err) console.log(err);
 
             if (stat.isFile(path.join(__dirname, 'files', files[index]))) {
-                console.log(path.basename(path.join(__dirname, 'files', files[index])));
                 input = fs.createReadStream(path.join(__dirname, 'files', path.basename(path.join(__dirname, 'files', files[index]))), 'utf-8');
                 output = fs.createWriteStream(path.join(__dirname, 'files-copy', path.basename(path.join(__dirname, 'files', files[index]))));
                 input.pipe(output);
